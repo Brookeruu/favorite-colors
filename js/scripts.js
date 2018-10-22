b9f4086d33d4e4cd6b37e3ab9fa7489d10e69351
brooke perry and ryan leslie <brooke@>
2018-10-22 10:28:34 -0700

    add push property

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,19 @@ $(function(){
     var color = $("input#color").val();
     var color1 = $("input#color1").val();
     var color2 = $("input#color2").val();
+    var color3 = $("input#color3").val();
     // var favanimal = $("input#animal").val();
     // var favnumber = $("input#number").val();
 
     var all = [color,color1,color2]
+    all.push(color3)
+    all.concat([color3]);
+    [color,color1,color2]
     $(".results").show();
 $("#clr").text(all[0]);
 $("#anml").text(all[1]);
 $("#numb").text(all[2]);
+$("#newresults").text(all);
+
 });
 });
