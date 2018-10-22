cca7676170b46096af6ca18b9ceb57d608d62599
brooke perry and ryan leslie <brooke@>
2018-10-22 09:24:22 -0700

    make arrays for js functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(function(){
   $("#form").submit(function(event){
     event.preventDefault();
-    
-  }
+    var favColor = $("input#color").val();
+    var favanimal = $("input#animal").val();
+    var favnumber = $("input#number").val();
+
+    var all = [favColor,favanimal,favnumber]
+    $(".results").show();
+;  }
 }
