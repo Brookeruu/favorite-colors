41871ad9f797838efbd2c4f5215b12c64f1497d4
brooke perry and ryan leslie <brooke@>
2018-10-22 10:05:50 -0700

    finish webpage with arrays and functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 $(function(){
   $("#form").submit(function(event){
     event.preventDefault();
-    var favColor = $("input#color").val();
-    var favanimal = $("input#animal").val();
-    var favnumber = $("input#number").val();
+    var color = $("input#color").val();
+    var color1 = $("input#color1").val();
+    var color2 = $("input#color2").val();
+    // var favanimal = $("input#animal").val();
+    // var favnumber = $("input#number").val();
 
-    var all = [favColor,favanimal,favnumber]
+    var all = [color,color1,color2]
     $(".results").show();
-;  }
-}
+$("#clr").text(all[0]);
+$("#anml").text(all[1]);
+$("#numb").text(all[2]);
+});
+});
